536431eef4ebed8d42f74d37f8dc619d86a063da
Kopano Mosai <kopano.mosai@gmail.com>
2026-04-11 02:37:17 +0200

    chore: replace hardcoded Gemini API key with placeholder in config

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     backendUrl: "http://localhost:3060/api",
-    geminiKey: 'AIzaSyCxXLG5_gHI1pje9b1TPr8klyvE03P5OGQ'
+    geminiKey: 'YOURKEYGEMINI'
 };
 
 console.log(`backendUrl in config.js: ${config.backendUrl}`)
