5c75ad758549aab3aa2b482c98c14dbfe89960cd
Kopano Mosai <kopano.mosai@gmail.com>
2026-04-11 02:35:51 +0200

    insight changes

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     backendUrl: "http://localhost:3060/api",
-    geminiKey: process.env.GEMINI_API_KEY
+    geminiKey: 'AIzaSyCxXLG5_gHI1pje9b1TPr8klyvE03P5OGQ'
 };
 
 console.log(`backendUrl in config.js: ${config.backendUrl}`)
